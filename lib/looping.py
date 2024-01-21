6972122e9c4ca78b9541e72c97998d9fe86348bb
Anthony <aquinoanthony851@gmail.com>
2024-01-20 17:01:57 -0700

    Control Flow: Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_integers = [number ** 2 for number in int_list]
+    return square_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if (num % 3 == 0) and (num % 5 == 0):
+            num = "FizzBuzz"
+        elif num % 3 == 0:
+            num = "Fizz"
+        elif num % 5 == 0:
+            num = "Buzz"
+        print(num)
+
+fizzbuzz()
